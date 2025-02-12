8305f24259d113ac082fce3a1071bb4b41a907b1
MarcoMirabal <marcomirabal.9@gmail.com>
2025-02-12 17:01:56 -0300

    12/02
    Estructurando la pagina

---
+++ administrador/template/cabecera.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <!-- Bootstrap CSS v5.2.1 -->
+     <link
+            href="../css/bootstrap.min.css"
+            rel="stylesheet"
+           
+        />
+
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+    <ul class="nav navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Administrador del Sitio</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Inicio</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Cursos</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Lecciones</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Cerrar</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Ver sitio web</a>
+        </li>
+
+    </ul>
+  </nav>
+
+
+
+  <div class="container">
+   <div class="row">
+
+
+
